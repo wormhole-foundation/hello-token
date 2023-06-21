e290eb01fb07eece8d67b4a46a4329fbdff7bffe
derpy-duck <115193320+derpy-duck@users.noreply.github.com>
2023-06-21 19:19:51 +0000

    move some files

--- test/HelloTokens.t.sol
+++ test/HelloTokens.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {HelloTokens, LiquidityProvided} from "../src/HelloTokens.sol";
+import {HelloTokens, LiquidityProvided} from "../src/example-extensions/HelloTokens.sol";
 
 import "wormhole-relayer-solidity-sdk/testing/WormholeRelayerTest.sol";
 
