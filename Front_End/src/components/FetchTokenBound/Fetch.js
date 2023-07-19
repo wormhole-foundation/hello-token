a0331d2e6441149482122a1adda016fc48c81139
iLVino <59415298+iLVino@users.noreply.github.com>
2023-07-20 00:08:19 +0200

    MVP1

--- Front_End/src/components/FetchTokenBound/Fetch.js
+++ Front_End/src/components/FetchTokenBound/Fetch.js
@@ -1,23 +1,15 @@
 import "./Fetch.css";
-import { abi_avalanche } from "./avalanche_abi";
-import { abi_moonbeam } from "./moonbeam_abi";
 
 import { create } from 'ipfs-http-client';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useConnect, useAccount } from 'wagmi';
 import { getPublicClient } from '@wagmi/core';
 import WalletConnect from "../WalletConnect/WalletConnect";
 
-import { prepareSendTransaction, sendTransaction } from '@wagmi/core';
-import { parseEther } from 'viem';
-import { writeContract } from '@wagmi/core';
-import { useContractReads } from 'wagmi';
-import { infuraProvider } from '@wagmi/core/providers/infura';
 import { ethers } from 'ethers';
 
-function Fetch () {
-    //const providesr = new ethers.providers.JsonRpcProvider("https://alpha-rpc.scroll.io/l2");
-    
+function Fetch() {
+
     const Contract_Address = "0x0ee7F43c91Ca54DEEFb58B261A454B9E8b4FEe8B";
     const mint_address = "0x7199D548f1B30EA083Fe668202fd5E621241CC89";
     const { connect, connectors, error, isLoading, pendingConnector, provider } = useConnect()
@@ -25,7 +17,7 @@ function Fetch () {
     const publicClient = getPublicClient();
     console.log(publicClient);
 
-    if(isConnected){
+    if (isConnected) {
         console.log("Provider is: ", provider)
     }
 
@@ -37,70 +29,122 @@ function Fetch () {
     const projectSecret = '6834cfa182ec09eeff6577aca368802e';
     const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
     const client = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    apiPath: '/api/v0',
-    headers: {
-        authorization: auth,
-    }
+        host: 'ipfs.infura.io',
+        port: 5001,
+        protocol: 'https',
+        apiPath: '/api/v0',
+        headers: {
+            authorization: auth,
+        }
     });
 
 
 
-    const moonbeamContractAddress = "";
-    const avalancheContractAddress = "";
+    const moonbeamContractAddress = "0x0591C25ebd0580E0d4F27A82Fc2e24E7489CB5e0";
+    const avalancheContractAddress = "0x04b80Cfd834085734E2783f0475829E195a5E812";
 
 
+    const [amountToSent, setAmountToSent] = useState(ethers.BigNumber.from(0));
 
-    async function mint() {
-        
-    }
+    const handleInputChange = (event) => {
+        const inputValue = event.target.value;
+        const newValue = ethers.BigNumber.from(inputValue);
+
+        // Ensure that the value is not negative before updating the state
+        if (newValue.gte(0)) {
+            setAmountToSent(newValue);
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        const keyCode = event.which || event.keyCode;
+        // Prevent entry of negative sign (minus) character (keycode: 45)
+        if (keyCode === 45) {
+            event.preventDefault();
+        }
+    };
+
+    const BridgeToken = async () => {
+        try {
+            // Token Bridge can only deal with 8 decimal places
+            // const formattedAmount = amountToSent.mul(10 ** 8);
+
+            // // Source and target chain IDs
+            // const sourceChain = 6;
+            // const targetChain = 16;
+            // const signer = provider.getSigner();
+
+            // // Get the source HT token contract
+            // const HTtoken = ERC20Mock__factory.connect(avalancheContractAddress, signer);
+
+            // const cost = ethers.utils.parseEther('0.1');
+
+            // // Approve the HelloToken contract to spend 'formattedAmount' of our HT token
+            // const approveTx = await HTtoken.approve(getHelloToken(sourceChain).address, formattedAmount);
+            // await approveTx.wait();
+
+            // console.log(`Sending ${formattedAmount.toString()} of the HT token`);
+
+            // // Send the tokens through the bridge
+            // const tx = await getHelloToken(sourceChain).sendCrossChainDeposit(
+            //     targetChain,
+            //     getHelloToken(targetChain).address,
+            //    // walletTargetChainAddress,
+            //     formattedAmount,
+            //     HTtoken.address,
+            //     { value: cost }
+            //);
+
+            // console.log(`Transaction hash: ${tx.hash}`);
+            // await tx.wait();
+            // console.log(`See transaction at: https://testnet.snowtrace.io/tx/${tx.hash}`);
+
+            // // Wait for some time (optional)
+            // await new Promise((resolve) => setTimeout(resolve, 30000));
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
 
 
-    async function Photogallery () 
-    {
-        
-    }
-
-    return(
+    return (
         <>
-        <div className="mybuttons">
-           
-            <h2 className="nam"> Intercontinentenal bridge </h2>
-        </div>
-            
             <div className="mybuttons">
-            <WalletConnect/>
-                <button className="btn" onClick={Photogallery}>NFT Minted</button>
+
+                <h2 className="nam"> Intercontinentenal bridge </h2>
+            </div>
+
+            <div className="mybuttons">
+                <WalletConnect />
             </div>
 
 
-
-            <button className="mintbutton" onClick={mint}>Mint</button>
-
-
-            {isConnected==false? <h2 className="h2center">
-            Connect wallet
+            {isConnected == false ? <h2 className="h2center">
+                Connect wallet
             </h2>
-            :
-            <>
-                
-                <div className="App">
-                <h2 className="h2cen">Wallet conneted successfully</h2>
-                </div>
-            </>
+                :
+                <>
+
+                    <div className="App">
+                        <h2 className="h2cen">Wallet conneted successfully</h2>
+                    </div>
+                </>
             }
 
-            
-            
-
-
-            <div className='nfts_container'>
-        </div>
+            <div>
+                <label htmlFor="amountInput" style={{ color: 'red' }}>Enter Amount to Bridge:</label>
+                <input
+                    type="number"
+                    id="amountInput"
+                    value={amountToSent.toString()}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                />
+            //<button className="btn" onClick={BridgeToken}>Bridge Token to Moonbeam</button>
+            </div>
 
         </>
     );
 }
-
 export default Fetch;
--- Front_End/src/components/FetchTokenBound/avalanche_abi.js
+++ Front_End/src/components/FetchTokenBound/avalanche_abi.js
@@ -1,441 +1,178 @@
 module.exports = {
     "abi_avalanche": [
-        {
-          "inputs": [],
-          "stateMutability": "nonpayable",
-          "type": "constructor"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "approved",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "Approval",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            },
-            {
-              "indexed": false,
-              "internalType": "bool",
-              "name": "approved",
-              "type": "bool"
-            }
-          ],
-          "name": "ApprovalForAll",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": false,
-              "internalType": "uint256",
-              "name": "_fromTokenId",
-              "type": "uint256"
-            },
-            {
-              "indexed": false,
-              "internalType": "uint256",
-              "name": "_toTokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "BatchMetadataUpdate",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": false,
-              "internalType": "uint256",
-              "name": "_tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "MetadataUpdate",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "previousOwner",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "newOwner",
-              "type": "address"
-            }
-          ],
-          "name": "OwnershipTransferred",
-          "type": "event"
-        },
-        {
-          "anonymous": false,
-          "inputs": [
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "indexed": true,
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "Transfer",
-          "type": "event"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "approve",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            }
-          ],
-          "name": "balanceOf",
-          "outputs": [
-            {
-              "internalType": "uint256",
-              "name": "",
-              "type": "uint256"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "getApproved",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "owner",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            }
-          ],
-          "name": "isApprovedForAll",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "name",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "owner",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "ownerOf",
-          "outputs": [
-            {
-              "internalType": "address",
-              "name": "",
-              "type": "address"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "renounceOwnership",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "string",
-              "name": "uri",
-              "type": "string"
-            }
-          ],
-          "name": "safeMint",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "safeTransferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            },
-            {
-              "internalType": "bytes",
-              "name": "data",
-              "type": "bytes"
-            }
-          ],
-          "name": "safeTransferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "operator",
-              "type": "address"
-            },
-            {
-              "internalType": "bool",
-              "name": "approved",
-              "type": "bool"
-            }
-          ],
-          "name": "setApprovalForAll",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "bytes4",
-              "name": "interfaceId",
-              "type": "bytes4"
-            }
-          ],
-          "name": "supportsInterface",
-          "outputs": [
-            {
-              "internalType": "bool",
-              "name": "",
-              "type": "bool"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [],
-          "name": "symbol",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "tokenURI",
-          "outputs": [
-            {
-              "internalType": "string",
-              "name": "",
-              "type": "string"
-            }
-          ],
-          "stateMutability": "view",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "from",
-              "type": "address"
-            },
-            {
-              "internalType": "address",
-              "name": "to",
-              "type": "address"
-            },
-            {
-              "internalType": "uint256",
-              "name": "tokenId",
-              "type": "uint256"
-            }
-          ],
-          "name": "transferFrom",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        },
-        {
-          "inputs": [
-            {
-              "internalType": "address",
-              "name": "newOwner",
-              "type": "address"
-            }
-          ],
-          "name": "transferOwnership",
-          "outputs": [],
-          "stateMutability": "nonpayable",
-          "type": "function"
-        }
-      ],
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "_wormholeRelayer",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "_tokenBridge",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "_wormhole",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "bytes32",
+            "name": "",
+            "type": "bytes32"
+          }
+        ],
+        "name": "NotAnEvmAddress",
+        "type": "error"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint16",
+            "name": "targetChain",
+            "type": "uint16"
+          }
+        ],
+        "name": "quoteCrossChainDeposit",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "cost",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "bytes",
+            "name": "payload",
+            "type": "bytes"
+          },
+          {
+            "internalType": "bytes[]",
+            "name": "additionalVaas",
+            "type": "bytes[]"
+          },
+          {
+            "internalType": "bytes32",
+            "name": "sourceAddress",
+            "type": "bytes32"
+          },
+          {
+            "internalType": "uint16",
+            "name": "sourceChain",
+            "type": "uint16"
+          },
+          {
+            "internalType": "bytes32",
+            "name": "deliveryHash",
+            "type": "bytes32"
+          }
+        ],
+        "name": "receiveWormholeMessages",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint16",
+            "name": "targetChain",
+            "type": "uint16"
+          },
+          {
+            "internalType": "address",
+            "name": "targetHelloToken",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "recipient",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          },
+          {
+            "internalType": "address",
+            "name": "token",
+            "type": "address"
+          }
+        ],
+        "name": "sendCrossChainDeposit",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "uint16",
+            "name": "sourceChain",
+            "type": "uint16"
+          },
+          {
+            "internalType": "bytes32",
+            "name": "sourceAddress",
+            "type": "bytes32"
+          }
+        ],
+        "name": "setRegisteredSender",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "tokenBridge",
+        "outputs": [
+          {
+            "internalType": "contract ITokenBridge",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "wormhole",
+        "outputs": [
+          {
+            "internalType": "contract IWormhole",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "wormholeRelayer",
+        "outputs": [
+          {
+            "internalType": "contract IWormholeRelayer",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ],
 }
